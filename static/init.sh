3062d22f95820d3c02966156172799a1f345227c
MikeMoolenaar <mmoolenaar9@gmail.com>
2024-01-17 20:21:22 +0100

    Add idiomorph + update init script

--- static/init.sh
+++ static/init.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # TODO should pick values from package.json
-HTMX_VERSION=1.9.9
+HTMX_VERSION=1.9.10
 HYPERSCRIPT_VERSION=0.9.12
+IDIOMORPH_VERSION=0.3.0
 
 # This script copies static files to the dist folder to node_modules
 
@@ -19,4 +20,6 @@ fi
 mkdir dist
 
 cp -r ./node_modules/htmx.org/dist/htmx.min.js "./dist/htmx-$HTMX_VERSION.min.js"
+cp -r ./node_modules/htmx.org/dist/ext/response-targets.js "./dist/htmx-response-targets-$HTMX_VERSION.min.js"
+cp -r ./node_modules/idiomorph/dist/idiomorph-ext.min.js "./dist/idiomorph-$IDIOMORPH_VERSION.min.js"
 cp -r ./node_modules/hyperscript.org/dist/_hyperscript.min.js "./dist/_hyperscript-$HYPERSCRIPT_VERSION.min.js"
