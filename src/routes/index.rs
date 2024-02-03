359a3206083eae2dc1f27bf44b03627b56c827f3
MikeMoolenaar <mmoolenaar9@gmail.com>
2024-02-03 21:44:04 +0100

    Add text to see how long the db query took, for fun

--- src/routes/index.rs
+++ src/routes/index.rs
@@ -9,7 +9,7 @@ use axum::{
 };
 use axum_htmx::HxBoosted;
 use minijinja::context;
-use std::sync::Arc;
+use std::{sync::Arc, time::Instant};
 use tower_sessions::Session;
 
 pub async fn index(session: Session, State(state): State<Arc<AppState>>, HxBoosted(boosted): HxBoosted) -> Response {
@@ -18,11 +18,14 @@ pub async fn index(session: Session, State(state): State<Arc<AppState>>, HxBoost
         return render_html("home.html", context!(), boosted).unwrap().into_response();
     }
     let user = session_user.unwrap();
+    let now = Instant::now();
     let todos: Vec<TodoItem> = sqlx::query_as!(TodoItem, "SELECT * FROM todos WHERE user_id = $1", user.id)
         .fetch_all(&state.db)
         .await
         .unwrap();
-    let context = context!(todos, user);
+    let elapsed = format!("{:?}", now.elapsed());
+
+    let context = context!(todos, user, elapsed);
     return render_html("home_todos.html", context, boosted)
         .unwrap()
         .into_response();
