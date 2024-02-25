bbaf88759bdd4da4af348942b4158c990678491c
MikeMoolenaar <mmoolenaar9@gmail.com>
2024-02-25 14:34:57 +0100

    Styling improvements

--- src/routes/error_404.rs
+++ src/routes/error_404.rs
@@ -1,15 +1,11 @@
-use crate::{render_html::*, AppState};
-use axum::{extract::State, http::StatusCode, response::Html};
+use crate::render_html::*;
+use axum::{http::StatusCode, response::Html};
 use axum_htmx::HxBoosted;
-use std::sync::Arc;
 
 pub async fn handle_static_404() -> (StatusCode, &'static str) {
     (StatusCode::NOT_FOUND, "Not found")
 }
 
-pub async fn handle_page_404(
-    State(_state): State<Arc<AppState>>,
-    HxBoosted(boosted): HxBoosted,
-) -> (StatusCode, Html<String>) {
+pub async fn handle_page_404(HxBoosted(boosted): HxBoosted) -> (StatusCode, Html<String>) {
     (StatusCode::NOT_FOUND, render_html("404.html", (), boosted).unwrap())
 }
